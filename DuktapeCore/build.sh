de117dec0af212d14195df2f03ba32ba93f9a88d
jianbai.gbj <jianbai.gbj@alibaba-inc.com>
2016-12-27 09:59:29 +0800

    update build.sh

--- DuktapeCore/build.sh
+++ DuktapeCore/build.sh
@@ -1,6 +1,6 @@
 gradle buildJar
 echo "Create Jar Path DuktapeEngine/build/libs/";
-cd DuktapeEngine/src/main/jni/
+cd DuktapeJava/src/main/jni/
 sh build.sh
 
 echo "Create So Path DuktapeEngine/src/main/libs/";
