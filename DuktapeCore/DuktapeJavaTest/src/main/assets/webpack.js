271c2f4e6b978fd724042b8225ca98eb2b228f37
jianbai.gbj <jianbai.gbj@alibaba-inc.com>
2017-03-22 17:15:38 +0800

    完善测试用例的严谨性

--- DuktapeCore/DuktapeJavaTest/src/main/assets/webpack.js
+++ DuktapeCore/DuktapeJavaTest/src/main/assets/webpack.js
@@ -1,6 +1,6 @@
-__webpack_require__ = function(a){print("webpack", a)};
-__webpack_require__(__webpack_require__.s = 2);
+__webpack_require__ = function(a){return "webpack" + a};
+a = __webpack_require__(__webpack_require__.s = 2);
 
 print("webpack done");
 
-true;
+"webpack2" == a;
